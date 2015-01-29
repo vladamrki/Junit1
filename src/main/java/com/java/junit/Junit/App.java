c1cfcc84d5d6f9ec901168081084ecef8b760e44
vladamrki <vladimirmrksic2013@gmail.com>
2015-01-29 17:58:05 +0100

    commit

---
+++ src/main/java/com/java/junit/Junit/App.java
@@ -0,0 +1,24 @@
+package com.java.junit.Junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static String getHelloWorld() {
+    	 
+		return "Hello World";
+ 
+	}
+ 
+	public static String getHelloWorld2() {
+ 
+		return "Hello World 2";
+ 
+	}
+}
